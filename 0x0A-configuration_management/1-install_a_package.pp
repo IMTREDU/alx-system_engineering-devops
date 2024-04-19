b1d585847b8137a82b215eb762a9d43bba001102
IMTREDU <imtr.edu@gmail.com>
2024-04-19 13:48:50 +0000

    Fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-# Puppet manifest to install Flask package from pip3
-package { 'flask':
-	ensure   => '2.1.0',
-	provider => 'pip3',
+# Puppet manifest to install Flask version 2.1.0 using pip3
+
+# Ensure the flask package is installed with version 2.1.0
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
