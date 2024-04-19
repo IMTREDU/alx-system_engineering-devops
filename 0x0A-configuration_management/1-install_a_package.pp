b268efe6331703f182107d4ae6a1b8cf4e61820d
IMTREDU <imtr.edu@gmail.com>
2024-04-19 13:30:07 +0000

    Check

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,7 @@
+#!/usr/bin/pup
+# Puppet manifest for installing Flask from pip3
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,5 @@
+# Puppet manifest to kill a process named killmenow
+
+exec { 'pkill killmenow':
+	path => '/usr/bin:/usr/sbin:/bin'
+}
