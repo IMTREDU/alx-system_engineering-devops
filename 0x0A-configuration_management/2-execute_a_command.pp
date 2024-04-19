7b17200d6687b511b8d5cc6621b60132c9f51728
IMTREDU <imtr.edu@gmail.com>
2024-04-19 13:53:10 +0000

    Fix

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,7 @@
-# Puppet manifest to kill a process named killmenow
-exec { 'pkill killmenow':
-	path => '/usr/bin/pkill killmenow',
+# Define an exec resource to kill the process named killmenow using pkill
+exec { 'killmenow':
+  command     => 'pkill killmenow',
+  path        => '/bin:/usr/bin:/sbin:/usr/sbin',
+  logoutput   => true,
+  refreshonly => true,
 }
