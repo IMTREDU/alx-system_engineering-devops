20b47c34ddbb2fdd4787ba63925d83435d72e512
IMTREDU <imtr.edu@gmail.com>
2024-04-19 13:57:30 +0000

    Fix?

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,8 @@
 # Define an exec resource to kill the process named killmenow using pkill
-exec { 'killmenow':
-  command     => 'pkill killmenow',
-  path        => '/bin:/usr/bin:/sbin:/usr/sbin',
-  logoutput   => true,
-  refreshonly => true,
+exec { 'killing a process using pkill':
+  command  => 'pkill -9 killmenow',
+  path     => '/usr/bin:/bin',
+  onlyif   => 'pgrep killmenow',
+  provider => shell,
+
 }
