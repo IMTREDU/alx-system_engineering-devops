225062159181212e55890193b4bbdcdbb3a25a82
IMTREDU <92468621+IMTREDU@users.noreply.github.com>
2025-02-26 01:50:42 +0200

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 """
-Module to query the Reddit API and return the num of subs
-for a given subreddit
+Script that queries subscribers on a given Reddit subreddit.
 """
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the num of subs for a given subreddit
-    If the subreddit is invalid, returns 0.
-    """
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
