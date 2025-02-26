0aa33e4b85e12c324358dde591919e20a78c685a
IMTREDU <imtr.edu@gmail.com>
2025-02-26 14:48:32 +0000

    Fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ Module to query the Reddit API & return the num of subs
 import requests
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API & Ret the total num of subs of given subreddit"""
+    """Queries the Reddit API & Ret the total num of subs of given subredit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
